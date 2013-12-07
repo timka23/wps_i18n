0a0e07f56c25c2ebcd8792de955a95046f140319
timka23 <timka23@tut.by>
2013-12-07 23:31:20 +0300

    Update wpsgallery.ts

--- ru_RU/ts/wpsgallery.ts
+++ ru_RU/ts/wpsgallery.ts
@@ -1,38 +1,38 @@
-<?xml version="1.0" encoding="utf-8"?>
+<?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE TS>
 <TS version="2.0" language="ru">
 <context>
     <name>KGalleryClient</name>
     <message>
         <source>gallery</source>
-        <translation type="unfinished">Галерея</translation>
+        <translation>Галерея</translation>
     </message>
     <message>
         <source>shape</source>
-        <translation type="unfinished">Форма</translation>
+        <translation>Форма</translation>
     </message>
 </context>
 <context>
     <name>KxDlgEditGallery</name>
     <message>
         <source>Caption:</source>
-        <translation type="unfinished">Надпись:</translation>
+        <translation>Надпись:</translation>
     </message>
     <message>
         <source>Directory:</source>
-        <translation type="unfinished">Каталог:</translation>
+        <translation>Каталог:</translation>
     </message>
     <message>
         <source>EditGallery</source>
-        <translation type="unfinished">EditGallery</translation>
+        <translation>EditGallery</translation>
     </message>
     <message>
         <source>From:</source>
-        <translation type="unfinished">От:</translation>
+        <translation>От:</translation>
     </message>
     <message>
         <source>Label:</source>
-        <translation type="unfinished">Метки:</translation>
+        <translation>Метки:</translation>
     </message>
     <message>
         <source>Less:</source>
@@ -40,56 +40,49 @@
     </message>
     <message>
         <source>More:</source>
-        <translation type="unfinished">Меньше:</translation>
+        <translation>Меньше:</translation>
     </message>
     <message>
         <source>Notes:</source>
-        <translation type="unfinished">Примечания:</translation>
+        <translation>Примечания:</translation>
     </message>
     <message>
         <source>OK</source>
-        <translation type="unfinished">Ок</translation>
+        <translation>Ок</translation>
     </message>
     <message>
         <source>Text</source>
-        <translation type="unfinished">Текст</translation>
+        <translation>Текст</translation>
     </message>
     <message>
         <source>The Most Number Of Caption Is 5.</source>
-        <translation type="unfinished">Наибольшее количество заголовок-5.</translation>
+        <translation>Наибольшее количество заголовок-5.</translation>
     </message>
 </context>
 <context>
     <name>KxEditGalleryDlg</name>
     <message>
         <source>Less</source>
-        <translation type="unfinished">Менее</translation>
+        <translation>Менее</translation>
     </message>
     <message>
         <source>More</source>
-        <translation type="unfinished">Более</translation>
+        <translation>Более</translation>
     </message>
 </context>
 <context>
     <name>QApplication</name>
     <message>
         <source>\Kingsoft Office\AppData\Desktop.ini name.</source>
-        <translation type="unfinished">Имя \Kingsoft Office\AppData\Desktop.ini.</translation>
+        <translation>Имя \Kingsoft Office\AppData\Desktop.ini.</translation>
     </message>
     <message>
         <source>\Kingsoft Office\AppData\My Materials\Desktop.ini name.</source>
-        <translation type="unfinished">\Kingsoft Office\AppData\My Materials\Desktop.ini имя.</translation>
+        <translation>\Kingsoft Office\AppData\My Materials\Desktop.ini имя.</translation>
     </message>
     <message>
         <source>\Kingsoft Office\Desktop.ini name.</source>
-        <translation type="unfinished">Имя \Kingsoft Office\Desktop.ini.</translation>
-    </message>
-</context>
-<context>
-    <name>WPSGalleryClass</name>
-    <message>
-        <source>WPSGallery</source>
-        <translation type="unfinished">WPSGallery</translation>
+        <translation>Имя \Kingsoft Office\Desktop.ini.</translation>
     </message>
 </context>
 </TS>
