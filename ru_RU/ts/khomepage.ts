87f097f0ef3f6fa8b026ca02ca1bcca236826368
timka23 <timka23@tut.by>
2013-12-07 22:52:44 +0300

    Update khomepage.ts

--- ru_RU/ts/khomepage.ts
+++ ru_RU/ts/khomepage.ts
@@ -1,52 +1,86 @@
-<?xml version="1.0" encoding="utf-8"?>
+<?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="sample">
+<TS version="2.0" language="ru">
 <context>
     <name>KHomepage</name>
     <message>
         <source>Failed in downloading &quot;%1&quot;.</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Ошибка при загрузке &quot;%1&quot;.</translation>
     </message>
 </context>
 <context>
     <name>KHomepageMainWindow</name>
     <message>
+        <source>&lt;a href=&quot;online&quot; style=&quot;text-decoration:none&quot;&gt;use new version&lt;/a&gt;</source>
+        <translation type="unfinished">&lt;a href=&quot;online&quot; style=&quot;text-decoration:none&quot;&gt;используйте новую версию&lt;/a&gt;</translation>
+    </message>
+    <message>
+        <source>Create blank document when startup</source>
+        <translation type="obsolete">Создайте пустой документ при запуске</translation>
+    </message>
+    <message>
+        <source>Homepage</source>
+        <translation type="unfinished">Домашняя страница</translation>
+    </message>
+    <message>
+        <source>Homepage</source>
+        <translation type="unfinished">Домашняя страница</translation>
+    </message>
+    <message>
+        <source>Homepage</source>
+        <translation type="unfinished">Домашняя страница</translation>
+    </message>
+    <message>
+        <source>current version:</source>
+        <translation type="obsolete">Текущая версия:</translation>
+    </message>
+    <message>
+        <source>restart to use newerversion</source>
+        <translation type="obsolete">Перезагрузить для использования более новой версии.</translation>
+    </message>
+    <message>
+        <source>version is latest</source>
+        <translation type="obsolete">Это последняя версия.</translation>
+    </message>
+    <message>
+        <source>version too old|</source>
+        <translation type="obsolete">Версия слишком стара.</translation>
+    </message>
+    <message>
         <source>Create a blank document at startup</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Создайте пустой документ при запуске</translation>
     </message>
     <message>
         <source>Current version:</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Текущая версия:</translation>
     </message>
     <message>
         <source>This is the latest version.</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Текущая версия:</translation>
     </message>
     <message>
         <source>Version is too old.</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Версия слишком стара.</translation>
     </message>
     <message>
         <source>&lt;a href=&quot;online&quot; style=&quot;text-decoration:none&quot;&gt;Use new version&lt;/a&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">&lt;a href=&quot;online&quot; style=&quot;text-decoration:none&quot;&gt;Используйте новую версию&lt;/a&gt;</translation>
     </message>
     <message>
         <source>Restart to use newer version.</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Перезагрузить для использования более новой версии.</translation>
     </message>
     <message>
         <source>Online Templates</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Шаблоны в Интернете</translation>
     </message>
     <message>
         <source>Check Online Templates</source>
-        <comment>tooltip</comment>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Подсказка</translation>
     </message>
     <message>
         <source>Online Templates</source>
-        <comment>title</comment>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Название</translation>
     </message>
 </context>
 </TS>
