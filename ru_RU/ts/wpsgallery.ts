b9f634b0db86a06ea53938f2437a2f51367de010
timka23 <timka23@tut.by>
2013-12-07 22:50:10 +0300

    Update wpsgallery.ts

--- ru_RU/ts/wpsgallery.ts
+++ ru_RU/ts/wpsgallery.ts
@@ -1,95 +1,95 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="ru_RU">
+<TS version="2.0" language="ru">
 <context>
     <name>KGalleryClient</name>
     <message>
         <source>gallery</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Галерея</translation>
     </message>
     <message>
         <source>shape</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Форма</translation>
     </message>
 </context>
 <context>
     <name>KxDlgEditGallery</name>
     <message>
-        <source>EditGallery</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>From:</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Text</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
         <source>Caption:</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Label:</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>The Most Number Of Caption Is 5.</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>More:</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>OK</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Надпись:</translation>
     </message>
     <message>
         <source>Directory:</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Каталог:</translation>
     </message>
     <message>
-        <source>Notes:</source>
-        <translation type="unfinished"></translation>
+        <source>EditGallery</source>
+        <translation type="unfinished">EditGallery</translation>
+    </message>
+    <message>
+        <source>From:</source>
+        <translation type="unfinished">От:</translation>
+    </message>
+    <message>
+        <source>Label:</source>
+        <translation type="unfinished">Метки:</translation>
     </message>
     <message>
         <source>Less:</source>
         <translation type="unfinished"></translation>
     </message>
+    <message>
+        <source>More:</source>
+        <translation type="unfinished">Меньше:</translation>
+    </message>
+    <message>
+        <source>Notes:</source>
+        <translation type="unfinished">Примечания:</translation>
+    </message>
+    <message>
+        <source>OK</source>
+        <translation type="unfinished">Ок</translation>
+    </message>
+    <message>
+        <source>Text</source>
+        <translation type="unfinished">Текст</translation>
+    </message>
+    <message>
+        <source>The Most Number Of Caption Is 5.</source>
+        <translation type="unfinished">Наибольшее количество заголовок-5.</translation>
+    </message>
 </context>
 <context>
     <name>KxEditGalleryDlg</name>
     <message>
-        <source>More</source>
-        <translation type="unfinished"></translation>
+        <source>Less</source>
+        <translation type="unfinished">Менее</translation>
     </message>
     <message>
-        <source>Less</source>
-        <translation type="unfinished"></translation>
+        <source>More</source>
+        <translation type="unfinished">Более</translation>
     </message>
 </context>
 <context>
     <name>QApplication</name>
     <message>
-        <source>\Kingsoft Office\Desktop.ini name.</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
         <source>\Kingsoft Office\AppData\Desktop.ini name.</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Имя \Kingsoft Office\AppData\Desktop.ini.</translation>
     </message>
     <message>
         <source>\Kingsoft Office\AppData\My Materials\Desktop.ini name.</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">\Kingsoft Office\AppData\My Materials\Desktop.ini имя.</translation>
+    </message>
+    <message>
+        <source>\Kingsoft Office\Desktop.ini name.</source>
+        <translation type="unfinished">Имя \Kingsoft Office\Desktop.ini.</translation>
     </message>
 </context>
 <context>
     <name>WPSGalleryClass</name>
     <message>
         <source>WPSGallery</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">WPSGallery</translation>
     </message>
 </context>
 </TS>
