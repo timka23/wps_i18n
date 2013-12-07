b8f7737f0786191ced8434bc9e914ac863471fc6
timka23 <timka23@tut.by>
2013-12-07 23:10:03 +0300

    Update wpp2doc.ts

--- ru_RU/ts/wpp2doc.ts
+++ ru_RU/ts/wpp2doc.ts
@@ -1,234 +1,345 @@
-<?xml version="1.0" encoding="utf-8"?>
+<?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="ru_RU">
+<TS version="2.0" language="ru">
 <context>
     <name>KTranslator</name>
     <message>
-        <source>Slide %1
-</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Notes Page:</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Being converted</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Still Need:</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
         <source> Minute</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Минута</translation>
     </message>
     <message>
-        <source>Second</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Ready to convert, please wait</source>
-        <translation type="unfinished"></translation>
+        <source> second</source>
+        <translation type="unfinished">второй</translation>
     </message>
     <message>
         <source>Being calculated</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Рассчитывается</translation>
+    </message>
+    <message>
+        <source>Being converted</source>
+        <translation type="unfinished">Конвертируется</translation>
+    </message>
+    <message>
+        <source>NotesPage:</source>
+        <translation type="unfinished">Страницы заметок:</translation>
+    </message>
+    <message>
+        <source>NotesPage: </source>
+        <translation type="unfinished">Страницы заметок:</translation>
+    </message>
+    <message>
+        <source>Ready To Convert,Please Wait</source>
+        <translation type="unfinished">Преобразую, пожалуйста подождите</translation>
+    </message>
+    <message>
+        <source>Slide %1 </source>
+    </message>
+    <message>
+        <source>Start Conversion</source>
+        <translation type="unfinished">Начало преобразования</translation>
+    </message>
+    <message>
+        <source>Still Need:</source>
+    </message>
+    <message>
+        <source>Notes Page:</source>
+        <translation type="unfinished">Страницы заметок:</translation>
+    </message>
+    <message>
+        <source>Second</source>
+    </message>
+    <message>
+        <source>Ready to convert, please wait</source>
     </message>
     <message>
         <source>Start converting</source>
-        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
     <name>ProcessDialog</name>
     <message>
-        <source>Translate to Document</source>
-        <translation type="unfinished"></translation>
+        <source>Cancel</source>
     </message>
     <message>
-        <source>Ready to convert</source>
-        <translation type="unfinished"></translation>
+        <source>Converted to the WPS Text document</source>
+        <translation type="unfinished">Преобразовать в текстовый документ WPS</translation>
+    </message>
+    <message>
+        <source>OpenFile</source>
+        <translation type="unfinished">Открытый файл</translation>
+    </message>
+    <message>
+        <source>Ready To Convert</source>
+        <translation type="unfinished">Преобразовать</translation>
     </message>
     <message>
         <source>Still Need:Being calculated</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">По-прежнему необходимо врямя для расчета</translation>
+    </message>
+    <message>
+        <source>Translate to Document</source>
+        <translation type="unfinished">Перевод документа</translation>
+    </message>
+    <message>
+        <source>Ready to convert</source>
     </message>
     <message>
         <source>Open File</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Cancel</source>
-        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
     <name>changeAsWpsDialog</name>
     <message>
-        <source>Translate to Document</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Select Slide</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>All(&amp;A)</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Current Slide(&amp;C)</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Selection(&amp;S)</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Slides(&amp;P):</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Converted Layout</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>According to original layout(&amp;O)</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Add notes to slide:</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Right(&amp;R)</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Below(&amp;B)</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Below(&amp;N)</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Right(&amp;H)</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Add blank line to slide:</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Slide per page(&amp;E):</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
         <source>1</source>
-        <translation type="unfinished"></translation>
     </message>
     <message>
         <source>2</source>
-        <translation type="unfinished"></translation>
     </message>
     <message>
         <source>3</source>
-        <translation type="unfinished"></translation>
     </message>
     <message>
         <source>4</source>
-        <translation type="unfinished"></translation>
     </message>
     <message>
-        <source>Number slides in the document</source>
-        <translation type="unfinished"></translation>
+        <source>According to original layout(&amp;O)</source>
+        <translation type="unfinished">Согласно оригинальному оформлению (&amp;O)</translation>
     </message>
     <message>
-        <source>Preview</source>
-        <translation type="unfinished"></translation>
+        <source>Add blank line to slide:</source>
+        <translation type="unfinished">Добавьте пустую строку к слайду:</translation>
+    </message>
+    <message>
+        <source>Add notes to slide:</source>
+        <translation type="unfinished">Добавьте заметки к слайду:</translation>
+    </message>
+    <message>
+        <source>All Slides(&amp;A)</source>
+        <translation type="unfinished">Все слайды (&amp;A)</translation>
+    </message>
+    <message>
+        <source>All(&amp;A)</source>
+    </message>
+    <message>
+        <source>Below(&amp;B)</source>
+        <translation type="unfinished">Ниже (&amp;B)</translation>
+    </message>
+    <message>
+        <source>Below(&amp;N)</source>
+        <translation type="unfinished">Ниже (&amp;N)</translation>
     </message>
     <message>
         <source>Conversion Includes</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Преобразования включает в себя</translation>
     </message>
     <message>
-        <source>Text(&amp;T)</source>
-        <translation type="unfinished"></translation>
+        <source>Conversion Includes Form(&amp;L)</source>
+        <translation type="unfinished">Преобразование включает форму (&amp;L)</translation>
     </message>
     <message>
-        <source>Table(&amp;L)</source>
-        <translation type="unfinished"></translation>
+        <source>Conversion Includes Picture(&amp;I)</source>
+        <translation type="unfinished">Преобразование включает картину (&amp;I)</translation>
+    </message>
+    <message>
+        <source>Conversion Includes Text(&amp;T)</source>
+        <translation type="unfinished">Преобразование включает текст (&amp;T)</translation>
+    </message>
+    <message>
+        <source>Converted Layout</source>
+        <translation type="unfinished">Преобразование включает текст (&amp;T)</translation>
+    </message>
+    <message>
+        <source>Converted to the WPS Text document</source>
+        <translation type="unfinished">Преобразованный в текстовый документ WPS</translation>
+    </message>
+    <message>
+        <source>Current Slide(&amp;C)</source>
+        <translation type="unfinished">Текущий слайд( &amp;C)</translation>
+    </message>
+    <message>
+        <source>Each Page Containing Slide(&amp;E):</source>
+        <translation type="unfinished">Каждая страница, содержащая слайд( &amp;E):</translation>
+    </message>
+    <message>
+        <source>Layout Preview</source>
+        <translation type="unfinished">Предварительный просмотр</translation>
+    </message>
+    <message>
+        <source>Number slides in the document</source>
+    </message>
+    <message>
+        <source>Original Layout Conversion(&amp;O)</source>
+        <translation type="unfinished">Преобразование оригинального оформления (&amp;O)</translation>
     </message>
     <message>
         <source>Picture(&amp;I)</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Картина (&amp;I)</translation>
+    </message>
+    <message>
+        <source>Preview</source>
+    </message>
+    <message>
+        <source>Remarks At The Bottom(&amp;B)</source>
+        <translation type="unfinished">Замечания в нижней части( &amp;B)</translation>
+    </message>
+    <message>
+        <source>Remarks On The Right(&amp;R)</source>
+        <translation type="unfinished">Замечания справа (&amp;R)</translation>
+    </message>
+    <message>
+        <source>Right(&amp;H)</source>
+        <translation type="unfinished">Право (&amp;H)</translation>
+    </message>
+    <message>
+        <source>Right(&amp;R)</source>
+        <translation type="unfinished">Право (&amp;R)</translation>
+    </message>
+    <message>
+        <source>Select Slide</source>
+        <translation type="unfinished">Выберите слайд</translation>
+    </message>
+    <message>
+        <source>Selected Slide(&amp;S)</source>
+        <translation type="unfinished">Выбранный слайд( &amp;S)</translation>
+    </message>
+    <message>
+        <source>Selection(&amp;S)</source>
+        <translation type="unfinished">Выбор( &amp;S)</translation>
+    </message>
+    <message>
+        <source>Slide Add Empty Line:</source>
+        <translation type="unfinished">Слайд добавить пустую строку:</translation>
+    </message>
+    <message>
+        <source>Slide Note Added:</source>
+        <translation type="unfinished">Добавлено примечание в слайд:</translation>
+    </message>
+    <message>
+        <source>Slide per page(&amp;E):</source>
+        <translation type="unfinished">Сдвинуть на страницу( &amp;E):</translation>
+    </message>
+    <message>
+        <source>Slide(&amp;P):</source>
+        <translation type="unfinished">Презентация (&amp; P):</translation>
+    </message>
+    <message>
+        <source>Slides(&amp;P):</source>
+        <translation type="unfinished">Слайды (&amp;P):</translation>
+    </message>
+    <message>
+        <source>Space In The Below(&amp;N)</source>
+        <translation type="unfinished">Пространство ниже( &amp;N)</translation>
+    </message>
+    <message>
+        <source>Space on the right(&amp;H)</source>
+        <translation type="unfinished">Пространства справа( &amp;H)</translation>
+    </message>
+    <message>
+        <source>Table(&amp;L)</source>
+        <translation type="unfinished">Таблица(&amp;L)</translation>
+    </message>
+    <message>
+        <source>Text(&amp;T)</source>
+        <translation type="unfinished">Текст (&amp;T)</translation>
+    </message>
+    <message>
+        <source>Translate to Document</source>
+        <translation type="unfinished">Перевести документ</translation>
     </message>
 </context>
 <context>
     <name>kxProgressdlg</name>
     <message>
-        <source>The conversion is completed.</source>
-        <translation type="unfinished"></translation>
+        <source>Close</source>
+        <translation type="unfinished">Закрыть</translation>
     </message>
     <message>
-        <source>Close</source>
-        <translation type="unfinished"></translation>
+        <source>Conversion To Kingsoft Writer Document Appear Error</source>
+        <translation type="unfinished">При конвертации в Kingsoft Writer документе появилось сообщение об ошибке</translation>
+    </message>
+    <message>
+        <source>The conversion is complete</source>
+        <translation type="unfinished">Преобразование завершено.</translation>
+    </message>
+    <message>
+        <source>The conversion is completed.</source>
     </message>
     <message>
         <source>Converting to Kingsoft Writer document encountered an error.</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">При конвертации в документа  Kingsoft Writer произошла ошибка.</translation>
     </message>
 </context>
 <context>
     <name>kxwpptodocdlg</name>
     <message>
+        <source>&quot;</source>
+    </message>
+    <message>
+        <source>Can Enter The 1-2 Integer</source>
+        <translation type="unfinished">Можно ввести целое число 1-2</translation>
+    </message>
+    <message>
+        <source>Can Enter The 1-4 Integer</source>
+        <translation type="unfinished">Можно ввести целое число 1-4</translation>
+    </message>
+    <message>
+        <source>Enter the slide number or range, for example :1,3,5-9</source>
+        <translation type="unfinished">Введите номер слайда или диапазон, например: 1, 3, 5-9</translation>
+    </message>
+    <message>
+        <source>Input errors.  					Please enter the slide number or range,  					for example :1,3,5-9 (slides, separated by commas,  					between consecutive slides can be used in a crossed connection).</source>
+        <translation type="unfinished">Ошибки ввода.  					Пожалуйста, введите номер слайда или диапазон, например: 1, 3, 5-9 (слайды, разделенных запятыми, между слайдах может использоваться в соединении перекрещенные).</translation>
+    </message>
+    <message>
         <source>Input slide number or slide range. For example, 1,3,5-9</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>The number must be 1, 2, 3, or 4.</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>This is a valid number.
-Please input slide number or slide range. 
-For example, 1,3,5-9.</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Kingsoft Presentation</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>The number must be 1 or 2.</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Введите номер слайда или слайд диапазон. К примеру 1,3,5-9</translation>
     </message>
     <message>
         <source>Kingsoft Writer Document(*.wps)</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Kingsoft Writer Document(*.wps)</translation>
     </message>
     <message>
         <source>Microsoft Word 97/2000/XP/2003 Document(*.doc)</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Microsoft Word 97/2000/XP/2003 Document(*.doc)</translation>
+    </message>
+    <message>
+        <source>The saving file &quot;</source>
+        <translation type="unfinished">Сохранение файла &quot;</translation>
+    </message>
+    <message>
+        <source>WPS Presentation</source>
+        <translation type="unfinished">Презентация WPS</translation>
+    </message>
+    <message>
+        <source>has the same name with an opened file. An opened file is using the same name. Please choose another path, or close the opened file.</source>
+        <translation type="unfinished">имеет то же имя с открытого файла. Открытый файл использует то же имя. Пожалуйста, выберите другой путь, или закрыть открытый файл.</translation>
+    </message>
+    <message>
+        <source>save file</source>
+        <translation type="unfinished">сохранить файл</translation>
+    </message>
+    <message>
+        <source>The number must be 1, 2, 3, or 4.</source>
+    </message>
+    <message>
+        <source>This is a valid number. Please input slide number or slide range.  For example, 1,3,5-9.</source>
+        <translation type="unfinished">Это является допустимым числом. Пожалуйста введите номер слайда или слайд диапазон.  К примеру 1,3,5-9.</translation>
+    </message>
+    <message>
+        <source>Kingsoft Presentation</source>
+    </message>
+    <message>
+        <source>The number must be 1 or 2.</source>
     </message>
     <message>
         <source>Save As</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Число должно быть 1 или 2.</translation>
     </message>
     <message>
         <source>The file &quot;</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>&quot;</source>
-        <translation type="unfinished"></translation>
     </message>
     <message>
         <source>already exists. You cannot save the file with the same name as another open file. Choose a different name, or close the open file before saving.</source>
-        <translation type="unfinished"></translation>
     </message>
 </context>
 </TS>
