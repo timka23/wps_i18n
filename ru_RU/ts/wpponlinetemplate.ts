24de285f4551ecb703bd39e30767b4f762b1040f
timka23 <timka23@tut.by>
2013-12-07 22:45:49 +0300

    Update wpponlinetemplate.ts

--- ru_RU/ts/wpponlinetemplate.ts
+++ ru_RU/ts/wpponlinetemplate.ts
@@ -1,56 +1,73 @@
-<?xml version="1.0" encoding="utf-8"?>
+<?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="ru_RU">
+<TS version="2.0" language="ru">
+<defaultcodec>UTF-8</defaultcodec>
 <context>
     <name>KLiveService</name>
     <message>
         <source>please install kuaipan manul</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Пожалуйста, установите kuaipan манул</translation>
     </message>
     <message>
         <source>please login kuaipan</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Войди в kuaipan</translation>
     </message>
 </context>
 <context>
     <name>KWppOnlineTemplate</name>
     <message>
+        <source>blank design</source>
+        <translation type="obsolete">Пустой дизайн</translation>
+    </message>
+    <message>
         <source>Blank Design</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Чистый дизайн</translation>
     </message>
 </context>
 <context>
     <name>KxTpOnlineTemplateWidget</name>
     <message>
-        <source>Using Template</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Online Templates</source>
-        <translation type="unfinished"></translation>
+        <source>Local Template</source>
+        <translation type="obsolete">Локальные шаблоны</translation>
     </message>
     <message>
         <source>My Favorit</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Мой любимый</translation>
+    </message>
+    <message>
+        <source>Online Template</source>
+        <translation type="obsolete">Шаблоны в Интернете</translation>
+    </message>
+    <message>
+        <source>USing Template</source>
+        <translation type="obsolete">Использование шаблона</translation>
+    </message>
+    <message>
+        <source>Using Template</source>
+        <translation type="unfinished">С помощью шаблона</translation>
+    </message>
+    <message>
+        <source>Online Templates</source>
+        <translation type="unfinished">Шаблоны в Интернете</translation>
     </message>
     <message>
         <source>Local Templates</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Локальные шаблоны</translation>
     </message>
 </context>
 <context>
     <name>QApplication</name>
     <message>
         <source>\Kingsoft Office\Desktop.ini name.</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">\Kingsoft Office\Desktop.ini name.</translation>
     </message>
     <message>
         <source>\Kingsoft Office\Office Space\Desktop.ini name.</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">\Kingsoft Office\Office Space\Desktop.ini name.</translation>
     </message>
     <message>
         <source>\Kingsoft Office\Office Space\My Templates\Desktop.ini name.</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">\Kingsoft Office\Office Space\My Templates\Desktop.ini имя.</translation>
     </message>
 </context>
 </TS>
