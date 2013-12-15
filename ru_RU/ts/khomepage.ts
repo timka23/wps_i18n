2b34e4ac21573c1adf3fa422a9c9470282e0eff0
timka23 <timka23@tut.by>
2013-12-15 22:04:06 +0300

    Update khomepage.ts

--- ru_RU/ts/khomepage.ts
+++ ru_RU/ts/khomepage.ts
@@ -1,8 +1,23 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="ru">
+<TS version="2.0" language="ru_RU">
+<context>
+    <name>KHomePageButtom</name>
+    <message>
+        <source>version is latest</source>
+        <translation>Это последняя версия</translation>
+    </message>
+    <message>
+        <source>version too old</source>
+        <translation>слишком старая версия</translation>
+    </message>
+</context>
 <context>
     <name>KHomepage</name>
+    <message>
+        <source>Downloading %1</source>
+        <translation>Загрузка %1</translation>
+    </message>
     <message>
         <source>Failed in downloading &quot;%1&quot;.</source>
         <translation>Ошибка при загрузке &quot;%1&quot;.</translation>
@@ -10,6 +25,49 @@
 </context>
 <context>
     <name>KHomepageMainWindow</name>
+    <message>
+        <source>&lt;a href=&quot;online&quot; style=&quot;text-decoration:none&quot;&gt;Use new version&lt;/a&gt;</source>
+        <translation>&lt;a href=&quot;online&quot; style=&quot;text-decoration:none&quot;&gt;Используйте новую версию&lt;/a&gt;</translation>
+    </message>
+    <message>
+        <source>Create a blank document at startup</source>
+        <translation>Создайте пустой документ при запуске</translation>
+    </message>
+    <message>
+        <source>Online Templates</source>
+        <translation>Шаблоны в Интернете</translation>
+    </message>
+    <message>
+        <source>Online Templates</source>
+        <translation>Шаблоны в Интернете</translation>
+    </message>
+    <message>
+        <source>Online Templates</source>
+        <translation>Шаблоны в Интернете</translation>
+    </message>
+    <message>
+        <source>Current version:</source>
+        <translation>Текущая версия:</translation>
+    </message>
+    <message>
+        <source>Restart to use newer version.</source>
+        <translation>Перезагрузить для использования более новой версии.</translation>
+    </message>
+    <message>
+        <source>This is the latest version.</source>
+        <translation>Текущая версия:.</translation>
+    </message>
+    <message>
+        <source>Version is too old.</source>
+        <translation>Версия слишком стара.</translation>
+    </message>
+    <message>
+        <source>Check Online Templates</source>
+        <translation>Подсказка</translation>
+    </message>
+</context>
+<context>
+    <name>KHomwPageButtom</name>
     <message>
         <source>&lt;a href=&quot;online&quot; style=&quot;text-decoration:none&quot;&gt;use new version&lt;/a&gt;</source>
         <translation>&lt;a href=&quot;online&quot; style=&quot;text-decoration:none&quot;&gt;используйте новую версию&lt;/a&gt;</translation>
@@ -19,68 +77,16 @@
         <translation>Создайте пустой документ при запуске</translation>
     </message>
     <message>
-        <source>Homepage</source>
-        <translation>Домашняя страница</translation>
-    </message>
-    <message>
-        <source>Homepage</source>
-        <translation>Домашняя страница</translation>
-    </message>
-    <message>
-        <source>Homepage</source>
-        <translation>Домашняя страница</translation>
-    </message>
-    <message>
         <source>current version:</source>
         <translation>Текущая версия:</translation>
     </message>
     <message>
-        <source>restart to use newerversion</source>
-        <translation>Перезагрузить для использования более новой версии.</translation>
-    </message>
-    <message>
         <source>version is latest</source>
-        <translation>Это последняя версия.</translation>
+        <translation>Это последняя версия</translation>
     </message>
     <message>
-        <source>version too old|</source>
-        <translation>Версия слишком стара.</translation>
-    </message>
-    <message>
-        <source>Create a blank document at startup</source>
-        <translation>Создайте пустой документ при запуске</translation>
-    </message>
-    <message>
-        <source>Current version:</source>
-        <translation>Текущая версия:</translation>
-    </message>
-    <message>
-        <source>This is the latest version.</source>
-        <translation>Текущая версия:</translation>
-    </message>
-    <message>
-        <source>Version is too old.</source>
-        <translation>Версия слишком стара.</translation>
-    </message>
-    <message>
-        <source>&lt;a href=&quot;online&quot; style=&quot;text-decoration:none&quot;&gt;Use new version&lt;/a&gt;</source>
-        <translation>&lt;a href=&quot;online&quot; style=&quot;text-decoration:none&quot;&gt;Используйте новую версию&lt;/a&gt;</translation>
-    </message>
-    <message>
-        <source>Restart to use newer version.</source>
-        <translation>Перезагрузить для использования более новой версии.</translation>
-    </message>
-    <message>
-        <source>Online Templates</source>
-        <translation>Шаблоны в Интернете</translation>
-    </message>
-    <message>
-        <source>Check Online Templates</source>
-        <translation>Подсказка</translation>
-    </message>
-    <message>
-        <source>Online Templates</source>
-        <translation>Название</translation>
+        <source>version too old</source>
+        <translation>слишком старая версия</translation>
     </message>
 </context>
 </TS>
