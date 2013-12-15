33ae170046e8c9912b3c702b4c03ff56cff5b8e9
timka23 <timka23@tut.by>
2013-12-15 22:05:42 +0300

    Update kole.ts

--- ru_RU/ts/kole.ts
+++ ru_RU/ts/kole.ts
@@ -1,11 +1,23 @@
-<?xml version="1.0" encoding="utf-8"?>
+<?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="ru">
+<TS version="2.0" language="ru_RU">
 <context>
     <name>KAxServer</name>
     <message>
+        <source>&quot;%1&quot; has been modified. Do you want to save it?</source>
+        <translation>&quot;%1&quot; был изменен. Вы хотите сохранить?</translation>
+    </message>
+    <message>
+        <source>%1 in %2</source>
+        <translation>%1 в%2</translation>
+    </message>
+    <message>
         <source>Do you want to save changes to the file?</source>
         <translation>Вы хотите сохранить изменения в файле?</translation>
+    </message>
+    <message>
+        <source>Untitled</source>
+        <translation>Без названия</translation>
     </message>
 </context>
 </TS>
