ff77f376b6f27f9f3c6a449882b8e31bbcf20175
timka23 <timka23@tut.by>
2013-12-15 22:07:43 +0300

    Update ktreasurebox.ts

--- ru_RU/ts/ktreasurebox.ts
+++ ru_RU/ts/ktreasurebox.ts
@@ -1,42 +1,27 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="ru">
+<TS version="2.0" language="ru_RU">
 <context>
     <name>KCommand</name>
     <message>
         <source>Treasure Box</source>
-        <translation type="unfinished">Коробка для сохранения</translation>
-    </message>
-    <message>
-        <source>Treasure_Box_Prompt_No</source>
-        <translation type="unfinished">Нет, спасибо.</translation>
-    </message>
-    <message>
-        <source>Treasure_Box_Prompt_Text</source>
-        <translation type="unfinished">Многие люди используют этот инструмент. Вы хотели бы попробовать?</translation>
-    </message>
-    <message>
-        <source>Treasure_Box_Prompt_Title</source>
-        <translation type="unfinished">Kingsoft Office рекомендации</translation>
-    </message>
-    <message>
-        <source>Treasure_Box_Prompt_Yes</source>
-        <translation type="unfinished">Все в порядке</translation>
-    </message>
-    <message>
-        <source>Kingsoft Office recommendation</source>
-    </message>
-    <message>
-        <source>Many people are using this great tool. Would you like to try it?</source>
-        <translation type="unfinished">Многие люди используют этот инструмент. Вы хотели бы попробовать?</translation>
-    </message>
-    <message>
-        <source>All right</source>
-        <translation type="unfinished">Все в порядке</translation>
+        <translation>Шкатулка</translation>
     </message>
     <message>
         <source>No thanks.</source>
-        <translation type="unfinished">Нет, спасибо.</translation>
+        <translation>Нет, спасибо.</translation>
+    </message>
+    <message>
+        <source>Many people are using this great tool. Would you like to try it?</source>
+        <translation>Многие люди используют этот замечательный инструмент. Хотите попробовать?</translation>
+    </message>
+    <message>
+        <source>Kingsoft Office recommendation</source>
+        <translation>Рекомендация Kingsoft Office</translation>
+    </message>
+    <message>
+        <source>All right</source>
+        <translation>Все в порядке</translation>
     </message>
 </context>
 </TS>
